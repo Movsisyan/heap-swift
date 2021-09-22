fd945284953679de2a72ca29cfa6ce21c003cab5
Movsisyan <mher.movsisyan@glovoapp.com>
2021-09-22 22:03:32 +0400

    Move private methods to dedicated extension

--- Sources/Heap/Heap.swift
+++ Sources/Heap/Heap.swift
@@ -6,42 +6,6 @@ public struct Heap<Element: Comparable> {
     items.first
   }
   
-  private func hasLeftChild(_ index: Int) -> Bool {
-    leftChildIndex(index) < items.count
-  }
-  
-  private func hasRightChild(_ index: Int) -> Bool {
-    rightChildIndex(index) < items.count
-  }
-  
-  private func hasParent(_ index: Int) -> Bool {
-    parentIndex(index) >= 0
-  }
-  
-  private func leftChildIndex(_ index: Int) -> Int {
-    2 * index + 1
-  }
-  
-  private func rightChildIndex(_ index: Int) -> Int {
-    2 * index + 2
-  }
-  
-  private func parentIndex(_ index: Int) -> Int {
-    (index - 1)/2
-  }
-  
-  private func leftChild(_ index: Int) -> Element {
-    items[leftChildIndex(index)]
-  }
-  
-  private func rightChild(_ index: Int) -> Element {
-    items[rightChildIndex(index)]
-  }
-  
-  private func parent(_ index: Int) -> Element {
-    items[parentIndex(index)]
-  }
-  
   private mutating func swapElementsAtIndexes(_ indexOne: Int, _ indexTwo: Int) {
     let temp = items[indexOne]
     items[indexOne] = items[indexTwo]
@@ -62,17 +26,58 @@ public struct Heap<Element: Comparable> {
     return item
   }
   
-  private mutating func heapifyDown() {
-    // TODO: - Implement heapifyDown
-  }
-  
   public mutating func push(_ item: Element) {
     items.append(item)
     heapifyUp()
   }
   
-  private mutating func heapifyUp() {
+}
+
+// MARK: - Private helpers
+
+private extension Heap {
+  
+  mutating func heapifyDown() {
+    // TODO: - Implement heapifyDown
+  }
+  
+  mutating func heapifyUp() {
     // TODO: - Implement heapifyUp
   }
   
- }
+  func hasLeftChild(_ index: Int) -> Bool {
+    leftChildIndex(index) < items.count
+  }
+  
+  func hasRightChild(_ index: Int) -> Bool {
+    rightChildIndex(index) < items.count
+  }
+  
+  func hasParent(_ index: Int) -> Bool {
+    parentIndex(index) >= 0
+  }
+  
+  func leftChildIndex(_ index: Int) -> Int {
+    2 * index + 1
+  }
+  
+  func rightChildIndex(_ index: Int) -> Int {
+    2 * index + 2
+  }
+  
+  func parentIndex(_ index: Int) -> Int {
+    (index - 1)/2
+  }
+  
+  func leftChild(_ index: Int) -> Element {
+    items[leftChildIndex(index)]
+  }
+  
+  func rightChild(_ index: Int) -> Element {
+    items[rightChildIndex(index)]
+  }
+  
+  func parent(_ index: Int) -> Element {
+    items[parentIndex(index)]
+  }
+}
