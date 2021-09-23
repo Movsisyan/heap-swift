007eef6343b25d0e9584d91f7ddaeec018bddac4
Movsisyan <mher.movsisyan@glovoapp.com>
2021-09-23 07:06:38 +0400

    Add `test_pushElementOnEmptyHeap` to the `HeapTests`

--- Tests/HeapTests/HeapTests.swift
+++ Tests/HeapTests/HeapTests.swift
@@ -6,7 +6,7 @@ final class HeapTests: XCTestCase {
   func test_containsNoElementOnInitialization() {
     let heap = Heap<Int>()
     
-    XCTAssertTrue(heap.isEmpty, "Heap must be empty after initilization")
+    XCTAssertTrue(heap.isEmpty, "The heap must be empty after initilization")
   }
   
   func test_peekElementOnEmptyHeap_shouldReturnNil() {
@@ -19,5 +19,15 @@ final class HeapTests: XCTestCase {
     var heap = Heap<Int>()
     
     XCTAssertNil(heap.pop(), "Empty heap must return nil on pop")
+  }
+  
+  func test_pushElementOnEmptyHeap() {
+    var heap = Heap<Int>()
+    let number = 4
+    heap.push(number)
+    
+    XCTAssertFalse(heap.isEmpty, "The heap must not be empty")
+    XCTAssertEqual(heap.count, 1, "The heap should contain one element")
+    XCTAssertEqual(heap.peek, number)
   }
 }
