e2b91b102b731629c06ed7f71b3c3925852c461e
Movsisyan <mher.movsisyan@glovoapp.com>
2021-09-23 06:51:09 +0400

    Add `test_PeekElementOnEmptyHeap_shouldReturnNil` test

--- Tests/HeapTests/HeapTests.swift
+++ Tests/HeapTests/HeapTests.swift
@@ -3,9 +3,15 @@ import XCTest
 
 final class HeapTests: XCTestCase {
   
-  func testContainsNoElementOnInitialization() {
+  func test_containsNoElementOnInitialization() {
     let heap = Heap<Int>()
     
-    XCTAssertTrue(heap.isEmpty, <#T##message: String##String#>)
+    XCTAssertTrue(heap.isEmpty, "Heap must be empty after initilization")
+  }
+  
+  func test_PeekElementOnEmptyHeap_shouldReturnNil() {
+    let heap = Heap<Int>()
+    
+    XCTAssertNil(heap.peek, "Empty heap must return nil on peek")
   }
 }
