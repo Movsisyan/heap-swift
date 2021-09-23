1baa5cbdb41387532c65825ff97dfa8c0eb0f1b7
Movsisyan <mher.movsisyan@glovoapp.com>
2021-09-23 07:30:19 +0400

    Add `test_minHeapReturnItemsInOrder` to the `HeapTests`

--- Tests/HeapTests/HeapTests.swift
+++ Tests/HeapTests/HeapTests.swift
@@ -3,6 +3,8 @@ import XCTest
 
 final class HeapTests: XCTestCase {
   
+  private let items = [2, -9, 34, 5, 1, 29, 6, -4, 0, 10, 10]
+  
   func test_containsNoElementOnInitialization() {
     let heap = Heap<Int>()
     
@@ -39,6 +41,23 @@ final class HeapTests: XCTestCase {
     let popedItem = heap.pop()
     
     XCTAssertTrue(heap.isEmpty, "The heap must be empty")
-    XCTAssertEqual(number, popedItem, "Poped item must be equal to the initially pushed one")
+    XCTAssertEqual(number, popedItem, "Popped item must be equal to the initially pushed one")
+  }
+  
+  func test_minHeapReturnItemsInOrder() {
+    var heap = Heap<Int>()
+    for item in items {
+      heap.push(item)
+    }
+    
+    var prevItem = Int.min
+    while !heap.isEmpty {
+      guard let popedItem = heap.pop() else {
+        XCTFail("The not empty heap should always return value on the pop operation")
+        return
+      }
+      XCTAssertTrue(prevItem <= popedItem, "The previous item must always be less than or equal to the new one in the min-heap")
+      prevItem = popedItem
+    }
   }
 }
